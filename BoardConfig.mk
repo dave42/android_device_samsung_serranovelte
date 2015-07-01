25c1fe07ee466520f452eceab9863b5fb63f0a5e
David Moore <davidm@j5int.com>
2015-07-01 16:21:37 +0200

    Various settings copied from serrano-common, switching the kernel from MSM8960 to MSM8916

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,6 +2,8 @@ USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
 -include vendor/samsung/serranovelte/BoardConfigVendor.mk
+# inherit from common msm8916
+-include device/samsung/msm8916-common/BoardConfigCommon.mk
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
@@ -19,6 +21,10 @@ BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/serranovelte/rootdir/fstab.qcom
+
+TARGET_USERIMAGES_USE_EXT4 := true
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
@@ -27,5 +33,55 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/samsung/serranovelte/kernel
+#TARGET_KERNEL_SOURCE         := kernel/samsung/msm8916-common
+#TARGET_KERNEL_CONFIG         := cyanogen_serranovelte_defconfig
+#TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+# NFC
+BOARD_HAVE_NFC := true
+
+# BLN
+BOARD_HAVE_GENERIC_BLN := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/serrano-common/bluetooth
+
+# Audio
+BOARD_HAVE_SAMSUNG_AUDIO := true
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_FLUENCE_FOR_VOIP := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+BOARD_USES_SEPERATED_HEADSET_MIC := true
+BOARD_USES_SEPERATED_VOICE_SPEAKER := true
+BOARD_USES_SEPERATED_VOIP := true
+QCOM_CSDCLIENT_ENABLED := false
+
+# Enable QCOM FM feature
+AUDIO_FEATURE_ENABLED_FM := true
+QCOM_FM_ENABLED := true
+BOARD_USES_SEPERATED_FM := true
+
+# Allow suspend in charge mode
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
+# Camera
+TARGET_NEED_CAMERA_ZSL := true
+TARGET_NEED_FFC_PICTURE_FIXUP := true
+TARGET_NEED_FFC_VIDEO_FIXUP := true
+TARGET_NEED_DISABLE_FACE_DETECTION := true
+TARGET_NEED_DISABLE_FACE_DETECTION_BOTH_CAMERAS := true
+
+# Build our own PowerHAL
+TARGET_POWERHAL_VARIANT :=
+
+# Custom RIL class
+BOARD_RIL_CLASS := ../../../device/samsung/serrano-common/ril/
+
+# CMHW
+BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
